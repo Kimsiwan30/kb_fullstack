c4f282324b6c6fd480227644c85689a979b29d20
김시완 <rkdlqdh112@naver.com>
2024-05-28 17:57:56 +0900

    vue 연습

---
+++ 05_vue/chapter07/vue-test/src/App-emit.vue
@@ -0,0 +1,29 @@
+<script setup>
+import SendNameComponent from './components/SendNameComponent.vue';
+</script>
+
+<template>
+  <div>
+    <h1>너의 이름은 {{ name }}</h1>
+    <SendNameComponent v-on:nameChange="NameChangeHandler" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      name: '',
+    };
+  },
+  methods: {
+    NameChangeHandler(e) {
+      console.log(e.name);
+      this.name = e.name;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ 05_vue/chapter07/vue-test/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import ColorEmit from './components/ColorEmit.vue';
+</script>
+
+<template>
+  <div :style="{ backgroundColor: color }">
+    <h1>저의 색상을 정해주세요!!</h1>
+    <ColorEmit @colorEmit="ColorChangedHandlerer" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      color: '',
+    };
+  },
+  methods: {
+    ColorChangedHandlerer(e) {
+      console.log(e.color);
+      this.color = e.color;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
