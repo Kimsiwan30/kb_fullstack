fbd12ad37d1adbfe207576b073d413909fbb461c
김시완 <rkdlqdh112@naver.com>
2024-06-07 14:03:33 +0900

    24-06-07 codingtest

---
+++ Study/coding_test/test1.js
@@ -0,0 +1,13 @@
+// function solution(num1, num2) {
+//   return Math.floor(num1 / num2);
+// }
+// console.log(solution(34, 9));
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// console.log(arr.filter((item) => item === 0)); //  조건에 맞는 값만 반환해요
+console.log(arr.filter((item, index) => index)); //  조건에 맞는 값만 반환해요
+
+// console.log(arr.map((item) => (item === 2 ? (arr[item] = 2) : ''))); //  조건과 상관없이 모든 값을 무조건 반환
+
+// forEach()
---
+++ Study/coding_test/test5.js
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120583
+
+const arr = [];
+function solution(arr, n) {
+  for (let i = 0; i < arr.length; i++) {
+    
+  }
+}
