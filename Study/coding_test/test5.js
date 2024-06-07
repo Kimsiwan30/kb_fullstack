5b70117933990b57ba5b49deef2747deac9e4a23
김시완 <rkdlqdh112@naver.com>
2024-06-07 17:52:40 +0900

    24-06-07 자바스크립 연습

--- Study/coding_test/test5.js
+++ Study/coding_test/test5.js
@@ -1,8 +1,14 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120583
 
-const arr = [];
-function solution(arr, n) {
-  for (let i = 0; i < arr.length; i++) {
-    
-  }
+// const arr = [];
+// function solution(arr, n) {
+//   for (let i = 0; i < arr.length; i++) {
+
+//   }
+// }
+
+function solution(array, n) {
+  let count = 0;
+  var answer = array.filter((item) => (item === n ? count++ : 0));
+  return count;
 }
