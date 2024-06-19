be97531c020145ec7960368a2b1ae9dc6855d9c6
김시완 <rkdlqdh112@naver.com>
2024-06-19 18:07:20 +0900

    240619 스캐너 커밋

--- 07_JAVA/study/src/afternoon/scanner/Scanner6Ex.java
+++ 07_JAVA/study/src/afternoon/scanner/Scanner6Ex.java
@@ -6,30 +6,33 @@ public class Scanner6Ex {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       int rand = (int)(Math.random()*100);
+        int rand = (int) (Math.random() * 100);
 
-       while (true){
-           System.out.println("0에서 99까지의 숫자를 입력 : ");
-           int input = sc.nextInt();
+        while (true){
+            System.out.print("숫자를 입력하세요 : ");
+            int x = sc.nextInt();
 
-           if (input > rand){
-               System.out.println("down");
-           } else if (input < rand){
-               System.out.println("up");
-           } else {
-               System.out.println("정답입니다. 랜덤 숫자는" + rand);
-               break;
-           }
-       }
-
-
-
-
-
-
-
-
-
+            if(x == rand){
+                System.out.println("정답입니다");break;
+            }
+            else if(x < rand){
+                System.out.println("down");
+            }
+            else {
+                System.out.println("up");
+            }
+        }
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
